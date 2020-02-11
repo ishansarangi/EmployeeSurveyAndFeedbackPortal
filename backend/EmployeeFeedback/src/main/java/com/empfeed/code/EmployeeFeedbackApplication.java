90f36150bb4ccb0de19280b00b8459b734904599
kumarkalyan93 <kkalyan3@asu.edu>
2020-02-10 22:20:21 -0700

    Task-21, Adding BoilerPlate code for backend

---
+++ backend/EmployeeFeedback/src/main/java/com/empfeed/code/EmployeeFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.empfeed.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeFeedbackApplication.class, args);
+	}
+
+}
