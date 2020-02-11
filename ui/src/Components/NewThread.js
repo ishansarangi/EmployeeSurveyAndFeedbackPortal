a169cf6c62afe53fef27c219b23247f1a22de4c0
Ishan Kumar Sarangi <ishan.sipun1@gmail.com>
2020-02-11 13:17:06 -0700

    Created the button and added the new thread component on a dialog box

--- ui/src/Components/NewThread.js
+++ ui/src/Components/NewThread.js
@@ -1,31 +1,108 @@
 import React from 'react';
-import Toolbar from '@material-ui/core/Toolbar'
-import {
-    FormControl,
-    TextField
-} from "@material-ui/core";
+import {withStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+import {FormControl, TextField} from "@material-ui/core";
+
+
+const styles = theme => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+    width: 'fit-content',
+  },
+  formControl: {
+    marginTop: theme.spacing(2),
+    minWidth: 120,
+  },
+  formControlLabel: {
+    marginTop: theme.spacing(1),
+  },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500]
+    },
+
+    floatingLabelFocusStyle: {
+      color: "green"
+  }
+});
+
+const DialogTitle = withStyles(styles)(props => {
+    const {
+        children,
+        classes,
+        onClose,
+        ...other
+    } = props;
+    return (
+        <MuiDialogTitle disableTypography
+            className={
+                classes.root
+            }
+            {...other}>
+            <Typography variant="h6">
+                {children}</Typography>
+            {
+            onClose ? (
+                <IconButton aria-label="close"
+                    className={
+                        classes.closeButton
+                    }
+                    onClick={onClose}>
+                    <CloseIcon/>
+                </IconButton>
+            ) : null
+        } </MuiDialogTitle>
+    );
+});
+
+const DialogContent = withStyles(theme => ({
+    root: {
+        padding: theme.spacing(2)
+    }
+}))(MuiDialogContent);
 
 const NewThread = () => {
-    return (
-        <div style={
-            {
-                display: "flex",
-                justifyContent: "center",
-                margin: 20,
-                padding: 20
-            }
-        }>
-            <form style={
-                {width: "50%"}
-            }>
-                <Toolbar>
-                    <h1>New Thread</h1>
-                </Toolbar>
+    const [open, setOpen] = React.useState(false);
 
-                <FormControl margin="normal" fullWidth>
-                    <TextField id="filled-basic" label="Subject" variant="filled"/>
-                </FormControl>
-            </form>
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button variant="outlined" color="primary"
+                onClick={handleClickOpen}>
+                Create New Thread
+            </Button>
+            <Dialog 
+                fullWidth={'xl'}
+                maxWidth={'sm'}
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}>
+                <DialogTitle id="customized-dialog-title"
+                    onClose={handleClose}>
+                    New Thread
+                </DialogTitle>
+                <DialogContent dividers>
+                    <FormControl margin="normal" fullWidth>
+                        <TextField id="filled-basic" label="Subject" variant="filled" floatingLabelFocusStyle={styles.floatingLabelFocusStyle}/>
+                    </FormControl>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
