e62cc9882d35dd3763131bea0843522cbaf8ac83
Alsha Samantaray <alsha1306.samantaray@gmail.com>
2020-02-25 23:43:23 -0700

    US_2_Tsk_58: Added employee button to navigate and user Context

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import NavBar from './Components/NavBar';
 import ManagerPane from './Components/ManagerPane';
 import PropTypes from 'prop-types';
+import Feedback from './Components/Feedback';
+import { UserContext } from './Components/UserContext';
 
 const App = (props) => {
+    const [page, setPageName] = useState("NotSet");
+    const handleClick = (name) => {
+        setPageName(name);
+        console.log("Page selected :" + name);
+    };
+
     return (
-        <div>
-            <NavBar/>
-            <ManagerPane props={props}/>
-        </div>
+        <UserContext.Provider value={page}>
+            <div >
+                <NavBar page={page} onClick={(name) => handleClick(name)} />
+                <ManagerPane props={props} />
+
+
+
+            </div>
+
+        </UserContext.Provider>
     );
 }
 
--- ui/src/Components/NavBar.js
+++ ui/src/Components/NavBar.js
@@ -5,9 +5,10 @@ import ListItemText from '@material-ui/core/ListItemText';
 import TypoGraphy from '@material-ui/core/Typography'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 const NavBar = props => {
-
     return (
         <AppBar position="static">
             <Toolbar>
@@ -31,9 +32,36 @@ const NavBar = props => {
                                 Feedback
                             </TypoGraphy>
                         </ListItemText>
+
                     </ListItem>
+
                 </List>
+
+                <Grid
+                    justify="space-between" // Add it here :)
+                    container
+                    spacing={2}
+                >
+                    <Grid item>
+                        <div>
+                            <Button id="manager" href="#text-buttons" color="inherit">
+                                Manager
+                            </Button>
+
+                        </div>
+                    </Grid>
+
+                    <Grid item>
+                        <div>
+                            <Button href="#text-buttons" color="inherit"
+                                onClick={() => props.onClick("employee")}>
+                                Employee
+                            </Button>
+                        </div>
+                    </Grid>
+                </Grid>
             </Toolbar>
+
         </AppBar>
 
     )
