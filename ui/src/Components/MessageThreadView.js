d57ef52bed06d43d4fa937015dd489bbf124f918
Kjha7 <kjha7@asu.edu>
2020-02-27 12:47:40 -0700

    US_4_Task_45_Create_Message_View

--- ui/src/Components/MessageThreadView.js
+++ ui/src/Components/MessageThreadView.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './message.css';
-import TextBox from './TextBox.js';
-
+import {all_thread_data} from '../data/TestData';
+import Message from './Message';
+import TextBox from './TextBox';
 const MessageThreadView = ({selectedThread}) => {
+  const createMessageView = () => {
+    return all_thread_data[selectedThread].messages.map(msg => {
+      return <Message msg={msg} />;
+    });
+  };
   const handleSubmit = () => {};
   return (
     <div class="chat-containter">
-      <div id="chat" class="chat"></div>
+      <div id="chat" class="chat">
+        {createMessageView()}
+      </div>
       <TextBox handleSubmit={handleSubmit} />
     </div>
   );
 };
-
 MessageThreadView.propTypes = {};
-
 export default MessageThreadView;
