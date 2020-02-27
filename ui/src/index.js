f7cf7412cd55bb6d938ff31b6a2b25e517121af1
Ishan Kumar Sarangi <ishan.sipun1@gmail.com>
2020-02-26 20:51:37 -0700

    US_#_Tsk_#: Code clean, restructured and nodes updated

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App';
+import App from './components/App';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
-import Feedback from './Components/Feedback';
+import Feedback from './components/Feedback';
 
 function NoMatch() {
     return (
