344d8a1c4d4f34ef1b0215b19d68f8bb9b5f0649
Kjha7 <kjha7@asu.edu>
2020-03-03 03:25:01 -0700

    US_62_T_69 Add recipient on message header

--- ui/src/Components/ChatHeader.js
+++ ui/src/Components/ChatHeader.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import ChatHeaderRecipient from './ChatHeaderRecipient';
 
 const ChatHeader = ({msg}) => {
-  return <div class="header-container">{msg.subject}</div>;
+  return (
+    <div class="header-container">
+      {msg.subject}
+      <div class="component-header">
+        <ChatHeaderRecipient msg={msg} />
+      </div>
+    </div>
+  );
 };
 
 ChatHeader.propTypes = {};
