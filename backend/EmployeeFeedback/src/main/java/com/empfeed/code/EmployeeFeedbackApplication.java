0bb6cdd51223b93b4788172b47ae0bbcdc06a38a
Kjha7 <kjha7@asu.edu>
2020-03-22 14:31:48 -0700

    US10_T92_Create_GraphQL_Mutation

--- backend/EmployeeFeedback/src/main/java/com/empfeed/code/EmployeeFeedbackApplication.java
+++ backend/EmployeeFeedback/src/main/java/com/empfeed/code/EmployeeFeedbackApplication.java
@@ -2,9 +2,22 @@ package com.empfeed.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.empfeed.code.repository.EmployeeRepository;
+import com.empfeed.code.repository.MessageRepository;
+import com.empfeed.code.repository.MessageThreadRepository;
+import com.empfeed.code.repository.TagRepository;
+import com.empfeed.code.resolver.Mutation;
 
 @SpringBootApplication
 public class EmployeeFeedbackApplication {
+
+	@Bean
+	public Mutation mutation(EmployeeRepository employeeRepository, MessageThreadRepository messageThreadRepository,
+			MessageRepository messageRepository, TagRepository tagRepository) {
+		return new Mutation(employeeRepository, messageThreadRepository, messageRepository, tagRepository);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeFeedbackApplication.class, args);
