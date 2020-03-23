ede5b01b54f5f675c38400d4e7ac0d015da6297d
Sabyasachi <mohantysabyasachi27@gmail.com>
2020-03-22 20:23:43 -0600

    US-10 Task90 Create java models for graphQL input schema

--- backend/EmployeeFeedback/src/main/java/com/empfeed/code/repository/MessageThreadRepository.java
+++ backend/EmployeeFeedback/src/main/java/com/empfeed/code/repository/MessageThreadRepository.java
@@ -1,17 +1,10 @@
 package com.empfeed.code.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-import com.empfeed.code.model.MessageThread;
+import com.empfeed.code.model.entity.MessageThread;
 
-public interface MessageThreadRepository extends JpaRepository<MessageThread, Integer> {
-
-	
-	List<MessageThread> findByCreatedBy(int createdBy);
-	
-	List<MessageThread> findBySentTo(int createdBy);
-	
+public interface MessageThreadRepository extends CrudRepository<MessageThread, Long> {
 }
--- backend/EmployeeFeedback/src/main/java/com/empfeed/code/resolver/Mutation.java
+++ backend/EmployeeFeedback/src/main/java/com/empfeed/code/resolver/Mutation.java
@@ -6,21 +6,14 @@ import com.empfeed.code.repository.MessageRepository;
 import com.empfeed.code.repository.MessageThreadRepository;
 import com.empfeed.code.repository.TagRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Mutation implements GraphQLMutationResolver {
 
 	private EmployeeRepository employeeRepository;
-
 	private MessageThreadRepository messageThreadRepository;
-
 	private MessageRepository messageRepository;
-
 	private TagRepository tagRepository;
-
-	public Mutation(EmployeeRepository employeeRepository, MessageThreadRepository messageThreadRepository,
-			MessageRepository messageRepository, TagRepository tagRepository) {
-		this.employeeRepository = employeeRepository;
-		this.messageThreadRepository = messageThreadRepository;
-		this.messageRepository = messageRepository;
-		this.tagRepository = tagRepository;
-	}
+	
 }
