21742fec43e62b9d400e94823ef937b0eb5c19c0
Alsha Samantaray <alsha1306.samantaray@gmail.com>
2020-04-07 22:25:10 +0530

    US_9_Tsk_124:Open manage tags upon clicking add tag

--- ui/src/Components/tags/AddTag.js
+++ ui/src/Components/tags/AddTag.js
@@ -1,16 +1,47 @@
-import React, {useState} from 'react';
-import {withStyles} from '@material-ui/core/styles';
-import {TextField} from '@material-ui/core';
+import React, { useState } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import {orange} from '@material-ui/core/colors';
+import { orange } from '@material-ui/core/colors';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import ColorPicker from './ColorPicker';
 
-const AddTag = ({handleCreateTag}) => {
+const styles = theme => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+    width: 'fit-content',
+  },
+  formControl: {
+    marginTop: theme.spacing(2),
+    minWidth: 120,
+  },
+  formControlLabel: {
+    marginTop: theme.spacing(1),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  separator: {
+    marginTop: theme.spacing(1),
+  },
+  popUpStyle: {
+    border: '1px solid black',
+    borderRadius: '5%',
+    backgroundColor: 'lightgrey',
+  },
+});
+
+
+const AddTag = ({ handleCreateTag }) => {
   const [text, setText] = useState('');
   const [color, setColor] = useState('#000000');
 
@@ -80,6 +111,13 @@ const AddTag = ({handleCreateTag}) => {
             Create
           </CreateButton>
         </ListItemSecondaryAction>
+        <TagContainer>
+          <TagTable>
+            <ColorPicker>
+              <ColorGrid></ColorGrid>
+            </ColorPicker>
+          </TagTable>
+        </TagContainer>
       </ListItem>
     </List>
   );
