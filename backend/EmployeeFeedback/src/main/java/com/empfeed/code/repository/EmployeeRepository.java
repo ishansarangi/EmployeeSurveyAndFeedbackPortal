54d58204eb764511505976de1075188a0fb6d672
Sabyasachi <mohantysabyasachi27@gmail.com>
2020-04-15 14:55:10 -0600

    US 128 Task137 Create a backend API to fetch the manager hierarchy and display them in the Create New Thread dialog box.

--- backend/EmployeeFeedback/src/main/java/com/empfeed/code/repository/EmployeeRepository.java
+++ backend/EmployeeFeedback/src/main/java/com/empfeed/code/repository/EmployeeRepository.java
@@ -7,10 +7,22 @@ import org.springframework.data.repository.query.Param;
 import com.empfeed.code.model.entity.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-	
+
 	@Query("from Employee where email=:emailId")
 	public Employee findEmployeeByEmail(@Param("emailId") String emailId);
-	
+
 	@Query("from Employee where employeeId=:employeeId")
 	public Iterable<Employee> findManager(@Param("employeeId") Long employeeId);
+
+	@Query(value = "WITH RECURSIVE ancestors(employee_id, manager_id, first_name, last_name,created_at, email, user_type,  lvl) AS ("
+			+ "	SELECT cat.employee_id, cat.manager_id, cat.first_name, cat.last_name, cat.created_at, cat.email, cat.user_type, 1 AS lvl "
+			+ "	FROM employee cat " 
+			+ " WHERE cat.employee_id= :employeeId"
+			+ "	UNION ALL "
+			+ "	SELECT parent.employee_id, parent.manager_id, parent.first_name, parent.last_name, parent.created_at, parent.email, parent.user_type, child.lvl + 1 AS lvl "
+			+ "	FROM employee parent JOIN ancestors child "
+			+ "	ON parent.employee_id = child.manager_id )"
+			+ "	SELECT employee_id,first_name, last_name, created_at, email, user_type, null as manager_id from ancestors where employee_id != :employeeId  ORDER BY lvl "
+			, nativeQuery = true)
+	public Iterable<Employee> findManagerHierarchy(@Param("employeeId") Long employeeId);
 }
