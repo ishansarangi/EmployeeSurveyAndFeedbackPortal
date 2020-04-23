22201147bb0db4e8c95d5757532e3ce8dbf439f3
Ishan Kumar Sarangi <ishan.sipun1@gmail.com>
2020-04-23 08:10:02 -0700

    US_129_Tsk_133: Added implementation for message read status on threads

--- ui/src/Components/threadview/ThreadItem.js
+++ ui/src/Components/threadview/ThreadItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {makeStyles, withStyles} from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
@@ -9,12 +9,42 @@ import FiberManualRecordSharpIcon from '@material-ui/icons/FiberManualRecordShar
 import moment from 'moment';
 import Chip from '@material-ui/core/Chip';
 
-const useStyles = makeStyles(theme => ({
-  inline: {
-    display: 'inline',
+const useStyles = makeStyles((theme) => ({
+  rootM: {
+    display: 'flex',
+  },
+  leftPart: {
+    width: '30px',
+  },
+  rightPart: {
+    marginLeft: '4px',
+    marginTop: '6px',
+  },
+  topSecionWithoutBadge: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  text: {
+    fontSize: '14px',
+    color: 'black',
+  },
+  date: {
+    color: 'grey',
+    fontSize: '12px',
+    float: 'left',
   },
   readIcon: {
     color: '#E87424',
+  },
+  preview: {
+    fontSize: '14px',
+    marginLeft: '30px',
+    textOverflow: 'ellipsis',
+    color: 'black',
+    maxLines: 3,
+  },
+  inline: {
+    display: 'inline',
   },
   chipDiv: {
     display: 'flex',
@@ -23,26 +53,55 @@ const useStyles = makeStyles(theme => ({
       margin: theme.spacing(0.5),
     },
     marginTop: '5px',
+    float: 'right',
   },
 }));
 
-const ListItemLink = props => {
+const ListItemLink = (props) => {
   return <ListItem button component="a" {...props} />;
 };
 
 const ThreadItem = ({thread, threadKey, selectedThread, readThread}) => {
-  const getFullName = employee => {
+  const getFullName = (employee) => {
     if (employee) return employee.firstName + ' ' + employee.lastName;
     else return 'Anonymous';
   };
 
-  const getDate = dateInput => {
+  const getDate = (dateInput) => {
     if (dateInput) {
       return moment(dateInput).format('YYYY-MM-DD h:mm A');
     }
   };
 
-  const ListItemAvatarCustom = withStyles(theme => ({
+  const getReadIcon = () => {
+    if (!thread.read) {
+      return (
+        <ListItemAvatarCustom>
+          <FiberManualRecordSharpIcon
+            fontSize="inherit"
+            style={{fontSize: '15px'}}
+            className={classes.readIcon}
+          />
+        </ListItemAvatarCustom>
+      );
+    }
+  };
+  const getHeader = () => {
+    return (
+      <div className={classes.rootM}>
+        <div className={classes.leftPart}>{getReadIcon()}</div>
+
+        <div className={classes.rightPart}>
+          <div className={classes.topSecionWithoutBadge}>
+            <div className={classes.text}>{thread.subject}</div>
+            <div className={classes.date}> {getDate(thread.latestDate)}</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const ListItemAvatarCustom = withStyles((theme) => ({
     root: {
       minWidth: '25px',
     },
@@ -79,43 +138,14 @@ const ThreadItem = ({thread, threadKey, selectedThread, readThread}) => {
         key={threadKey}
         selected={selectedThread === threadKey}
       >
-        <ListItemAvatarCustom>
-          <FiberManualRecordSharpIcon
-            fontSize="inherit"
-            style={{fontSize: '20px'}}
-            className={classes.readIcon}
-          />
-        </ListItemAvatarCustom>
         <ListItemText
-          primary={
-            <>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textSecondary"
-              >
-                {getFullName(thread.createdBy)}
-              </Typography>
-
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textSecondary"
-                style={{float: 'right'}}
-              >
-                {getDate(thread.latestDate)}
-              </Typography>
-            </>
-          }
+          primary={<>{getHeader()}</>}
           secondary={
             <React.Fragment>
               <Typography
-                component="span"
-                variant="subtitle1"
-                className={classes.inline}
-                color="textPrimary"
+                className={classes.preview}
+                style={{wordWrap: 'break-word'}}
+                maxLines={3}
               >
                 {thread.latestText}
               </Typography>
